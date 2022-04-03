a32b8f67caddf320bcab78241dc9a7904f8af66f
Adeyiwunmi Adebolu <>
2022-04-03 15:12:37 +0100

    updated reeadme

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   /**
-   * takes in nothing
+   * takes in nothing, or sometimes all you haveeeeee
    * @returns Striing
    */
   @Get()
