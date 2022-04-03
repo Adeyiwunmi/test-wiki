6137017f88c661d17044ccabffeef06b97184531
Adeyiwunmi Adebolu <>
2022-04-03 14:41:20 +0100

    updated stuff

---
+++ src/hackers/hackers.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('hackers')
+export class HackersController {}
---
+++ src/preys/preys.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('preys')
+export class PreysController {}
